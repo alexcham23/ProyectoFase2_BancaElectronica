c180254ac3b7b3cd6f989f70b61f6fd0ab6ef08d
alexcham23 <tec-james.armira@outlook.com>
2020-12-23 15:12:25 -0600

    update

--- BancaElectronica/Admin/urls.py
+++ BancaElectronica/Admin/urls.py
@@ -5,5 +5,8 @@ from Admin import views
 urlpatterns=[
     path('',views.admon, name='ahora'),
     path('Create-Client-People/', views.persona ,name='Persona'),
-    path('Create-Client-Company',views.empresa,name='Company')
+    path('Create-Client-Company/',views.empresa,name='Company'),
+    path('Cuenta-Monetaria/',views.monetaria,name='Cmonetaria'),
+    path('Cuenta-Ahorro/',views.ahorro,name='Cahorro'),
+    path('Cuenta-Plazo-Fijo/',views.PlazoFijo,name='Cplazo'),
 ]
--- BancaElectronica/BancaElectronica/settings.py
+++ BancaElectronica/BancaElectronica/settings.py
@@ -114,9 +114,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-GT'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Guatemala'
 
 USE_I18N = True
 
@@ -124,7 +124,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-
+#fecha modificada
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
